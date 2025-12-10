c50d9bca3c739654bf7b95f585127cabbb2fa018
Hinkal0 <daniilmandrykin0@gmail.com>
2025-12-11 02:20:26 +0300

    Основная логика программы

--- main.c
+++ main.c
@@ -1,5 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#include "operations.h"
 
 int main(int argc, char** argv) {
+  if (argc != 3) {
+    printf("Неверное число аргументов!\n");
+    return EXIT_FAILURE;
+  }
 
+  Operation op = parseInput(argv[2]);
+  if (op.type == OPERATION_UNKNOWN) {
+    printf("Не удалось распознать команду!\n");
+    return EXIT_FAILURE;
+  }
+
+  if (execute(op, argv[1]) != EXIT_SUCCESS) {
+    printf("Не удалось выполнить операцию!\n");
+    freeOperation(op);
+    return EXIT_FAILURE;
+  }
+
+  freeOperation(op);
+
+  return EXIT_SUCCESS;
 }
